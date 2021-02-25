967bfe42521b2372e9e31d2fc2a9b1e73ae32b71
18485888 <AAlUstinov@sberbank.ru>
2021-02-25 14:01:50 +0300

    Почта

--- src/main/java/ru/job4j/collection/EmailExam.java
+++ src/main/java/ru/job4j/collection/EmailExam.java
@@ -1,11 +1,59 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class EmailExam {
     private Map<String, User> container = new HashMap<>();
+
+    public void addUser(User user) {
+        Predicate<String> predicate = (el) -> Arrays.stream(el.split(","))
+                .anyMatch(userEmail -> user.emails.contains(userEmail));
+        List<String> existUser = container.keySet()
+                .stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+        for (String us : existUser) {
+            //System.out.println(us);
+        }
+        //System.out.println("---------");
+        if (existUser.size() == 1) {
+            //System.out.println("Get it");
+            String keyInitialUser = existUser.iterator().next();
+            User initialUser = container.get(keyInitialUser);
+            for (String userEmails : user.emails) {
+                initialUser.addEmail(userEmails);
+            }
+            System.out.println(initialUser);
+            container.put(keyInitialUser, initialUser);
+            return;
+        }
+        container.put(String.join(",", user.emails), user);
+    }
+
+    public Map<String, User> getContainer() {
+        return container;
+    }
+
+    public User authorize(String email) {
+        Predicate<String> predicate = (el) -> el.contains(email);
+       List<String> existUser = container.keySet()
+               .stream()
+               .filter(predicate)
+               .collect(Collectors.toList());
+       if (existUser.size() == 2) {
+           User initialUser = container.get(existUser.iterator().next());
+           String keyJoinUser = existUser.iterator().next();
+           User joinUser = container.get(keyJoinUser);
+           for (String userEmails : joinUser.emails) {
+               initialUser.addEmail(userEmails);
+           }
+           container.put(keyJoinUser, null);
+           return initialUser;
+       }
+       return container.get(existUser.iterator().next());
+    }
 
     class User {
         private ArrayList<String> emails = new ArrayList();
@@ -15,11 +63,22 @@ public class EmailExam {
         }
 
         public boolean addEmail(String email) {
+            //System.out.println("email  " + email);
+            //System.out.println("emails  " + emails);
             if (!emails.contains(email)) {
+                //System.out.println("!!!!!!!");
                 emails.add(email);
+                //System.out.println("emails  After" + emails);
                 return true;
             }
             return false;
+        }
+
+        @Override
+        public String toString() {
+            return "User{"
+                    + "emails=" + emails
+                    + '}';
         }
 
         public ArrayList<String> getEmails() {
---
+++ src/test/java/ru/job4j/collection/EmailExamTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EmailExamTest {
+    @Test
+    public void whenAddThenGet() {
+        EmailExam emailExam = new EmailExam();
+        ArrayList<String> user1Emails = new ArrayList<>();
+        user1Emails.add("xxx@ya.ru");
+        user1Emails.add("foo@gmail.com");
+        user1Emails.add("lol@mail.ru");
+        ArrayList<String> user2Emails = new ArrayList<>();
+        user2Emails.add("foo@gmail.com");
+        user2Emails.add("ups@pisem.net");
+        ArrayList<String> user3Emails = new ArrayList<>();
+        user3Emails.add("xyz@pisem.net");
+        user3Emails.add("vasya@pupkin.com");
+        ArrayList<String> user4Emails = new ArrayList<>();
+        user4Emails.add("ups@pisem.net");
+        user4Emails.add("aaa@bbb.ru");
+        ArrayList<String> user5Emails = new ArrayList<>();
+        user5Emails.add("xyz@pisem.net");
+
+        //EmailExam.User user1 = new EmailExam.User(Arrays.asList("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru"));
+        EmailExam.User user1 = emailExam.new User(user1Emails);
+        EmailExam.User user2 = emailExam.new User(user2Emails);
+        EmailExam.User user3 = emailExam.new User(user3Emails);
+        EmailExam.User user4 = emailExam.new User(user4Emails);
+        EmailExam.User user5 = emailExam.new User(user5Emails);
+
+        emailExam.addUser(user1);
+        emailExam.addUser(user2);
+        emailExam.addUser(user3);
+        emailExam.addUser(user4);
+        emailExam.addUser(user5);
+
+        Map<String, EmailExam.User> test = emailExam.getContainer();
+
+        for (Map.Entry<String, EmailExam.User> u : test.entrySet()) {
+            //System.out.println(" key "+ u.getKey() + " value "+u.getValue());
+        }
+        //System.out.println("user1 " + user1);
+        assertThat(test.size(), is(2));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        String rsl = array.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+}
