bb52f0cb1c02174f5d03a220ab046d2cb5c281a9
18485888 <AAlUstinov@sberbank.ru>
2021-03-13 02:02:53 +0300

    Сканирование файловой системы

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -1,13 +1,16 @@
 package ru.job4j.io;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class SearchFiles {
+public class SearchFiles extends SimpleFileVisitor<Path> {
     private Predicate predicate;
     private Path root;
     //private PrintFiles printFiles = new PrintFiles();
@@ -17,12 +20,19 @@ public class SearchFiles {
         this.root = root;
     }
 
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        return super.visitFile(file, attrs);
+    }
+
     public List<Path> getPaths() throws IOException {
-        Path tree = Files.walkFileTree(root, new PrintFiles());
-        //Path way = root.toAbsolutePath();
-        System.out.println(tree);
         List<Path> result = new ArrayList();
-        Path path = tree.getFileName();
+
+        //Path tree = visitFile(root);
+        //Path way = root.toAbsolutePath();
+        Path items = Files.walkFileTree(root, new PrintFiles());
+        Path path = items.getFileName();
+        System.out.println(path);
         if (predicate.test(path)) {
             System.out.println(path);
             result.add(path);
