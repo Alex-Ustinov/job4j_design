028421a34ad79c454ba9376c32034235ecacde2f
18485888 <AAlUstinov@sberbank.ru>
2021-03-17 02:28:51 +0300

    Архивировать проект

--- src/main/java/ru/job4j/io/ArgZip.java
+++ src/main/java/ru/job4j/io/ArgZip.java
@@ -1,15 +1,34 @@
 package ru.job4j.io;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.function.Predicate;
+
 public class ArgZip {
 
     private final String[] args;
 
     public ArgZip(String[] args) {
-        this.args = args;
+        String[] bunchArgs = args[0].split(" ");
+        int index = 0;
+        String[] initialArgs = new String[100];
+        for (String arg : bunchArgs) {
+            args[index++] = arg;
+        }
+        this.args = initialArgs;
+        valid();
     }
 
     public boolean valid() {
-        return false;
+        for (String arg : args) {
+            String[] keyVal = arg.split("=");
+            if (keyVal.length < 2) {
+                System.out.println("There are not enough params");
+                return false;
+            }
+        }
+        return true;
     }
 
     public String directory() {
@@ -21,6 +40,19 @@ public class ArgZip {
     }
 
     public String output() {
+        String Way = "./";
+        String condition = "";
+        for (String arg : args) {
+            String[] keyVal = arg.split("=");
+            if (keyVal[0] == "-d") {
+                Way = keyVal[1];
+            }
+            if (keyVal[0] == "-e") {
+                condition = keyVal[1];
+            }
+        }
+        Path path = Paths.get(Way);
+        Predicate<Path> predicate = p -> p.toFile().getName().endsWith(condition);
         return null;
     }
 }
