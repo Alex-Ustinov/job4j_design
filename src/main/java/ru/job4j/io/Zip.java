fea73fe5bab08232338c627443809733fa51772b
18485888 <AAlUstinov@sberbank.ru>
2021-03-19 00:52:02 +0300

    Архивировать проект refactoring

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -12,9 +12,15 @@ import java.util.function.Predicate;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-        Zip zip = new Zip();
-        for (File sours : sources) {
-            zip.packSingleFile(sours, target);
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -29,22 +35,22 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        new Zip().packSingleFile(
-                new File("./chapter_005/pom.xml"),
-                new File("./chapter_005/pom.zip")
-        );
-        //new String[] {"-d=c:\\project\\job4j\\", "-e=class", "-o=project.zip"}
-        ArgZip argZip = new ArgZip(args);
-        Path path = Paths.get(argZip.directory());
-        Predicate<Path> predicate = p -> p.toFile().getName().endsWith(argZip.exclude());
+    public static List<File> getFilesList(Path path, ArgZip argZip) throws IOException {
+        Predicate<Path> predicate = p -> !p.toFile().getName().endsWith(argZip.exclude());
         List<Path> listFiles = Search.search(path, predicate);
         List<File> arrFiles = new ArrayList<File>();
-        File targetFile = new File(argZip.output());
         for (Path pathFile : listFiles) {
             File file = Paths.get(pathFile.toString()).toFile();
             arrFiles.add(file);
         }
+        return arrFiles;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgZip argZip = new ArgZip(args);
+        Path path = Paths.get(argZip.directory());
+        File targetFile = new File(argZip.output());
+        List<File> arrFiles = getFilesList(path, argZip);
         new Zip().packFiles(arrFiles, targetFile);
     }
 }
