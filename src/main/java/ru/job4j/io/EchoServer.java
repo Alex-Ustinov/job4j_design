bb085ce619bc5d2525bb1e2daf6843667f5448dc
18485888 <AAlUstinov@sberbank.ru>
2021-03-23 02:05:59 +0300

    Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,4 @@
 package ru.job4j.io;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,16 +12,32 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String answer = null;
                     while (!(str = in.readLine()).isEmpty()) {
                         if (str.contains("msg=Bye")) {
                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                             server.close();
+                        } else if (str.contains("msg=Hello")) {
+                            answer = "Hello";
+                        } else if (str.contains("msg=Any")) {
+                            answer = "What";
                         }
                         System.out.println(str);
                     }
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                 }
             }
         }
     }
 }
+
+
+//                    if (str.contains("msg=Exit")) {
+//                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+//                        server.close();
+//                    } else if (str.contains("msg=Any")) {
+//                        out.write("What.".getBytes());
+//                    } else if (str.contains("msg=Hello")) {
+//                        out.write("Hello.".getBytes());
+//                    }
