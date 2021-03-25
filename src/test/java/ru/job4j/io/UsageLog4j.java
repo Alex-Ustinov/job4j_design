ce8ab61f89779dc2afdb96fede8367ac53c98e37
18485888 <AAlUstinov@sberbank.ru>
2021-03-26 01:27:16 +0300

    Slf4j - вывод переменных

--- src/test/java/ru/job4j/io/UsageLog4j.java
+++ src/test/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,14 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Alex";
+        int age = 32;
+        boolean isWork = true;
+        double salary = 1.33;
+        float location = (float) 7.836849;
+        byte id = 97;
+        long phoneNumber = 213123123;
+        LOG.debug("User info name : {}, id: {}, age : {}", name, id, age);
+        LOG.debug("isWork : {} , salary : {}, phoneNumber : {} location : {}", isWork, salary, phoneNumber, location);
     }
 }
