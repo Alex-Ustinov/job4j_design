b695aba70861576695c61feb617e40e708b5ff3c
18485888 <AAlUstinov@sberbank.ru>
2021-03-29 02:12:40 +0300

    JAXB. Преобразование XML в POJO.

--- src/main/java/ru/job4j/serialization/java/Car.java
+++ src/main/java/ru/job4j/serialization/java/Car.java
@@ -1,17 +1,32 @@
 package ru.job4j.serialization.java;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Car implements Serializable {
+    @XmlAttribute
     private int speed;
+    @XmlAttribute
     private boolean isSell;
+    @XmlAttribute
     private String type;
     private Wheel spareWheels;
     private Wheel[] wheels;
+
+    Car() {   }
 
     Car(int speed, Boolean isSell, String type, Wheel spareWheels, Wheel[] wheels) {
         this.speed = speed;
@@ -32,7 +47,7 @@ public class Car implements Serializable {
                + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         Wheel spareWheels = new Wheel("protected");
         Wheel[] wheels =  new Wheel[] {new Wheel("plain")};
         final Car serializedCar = new Car(111, false, "Honda", spareWheels, wheels);
@@ -56,5 +71,22 @@ public class Car implements Serializable {
                 + '}';
         final Car routeBackCar = gson.fromJson(jsonCar, Car.class);
         System.out.println(routeBackCar);
+
+        JAXBContext context = JAXBContext.newInstance(Car.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(serializedCar, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Car result = (Car) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
--- src/main/java/ru/job4j/serialization/java/Wheel.java
+++ src/main/java/ru/job4j/serialization/java/Wheel.java
@@ -1,8 +1,12 @@
 package ru.job4j.serialization.java;
 
 import java.io.Serializable;
+import com.sun.xml.txw2.annotation.XmlElement;
+import javax.xml.bind.annotation.*;
 
+@XmlElement(value = "wheel")
 public class Wheel implements Serializable {
+    @XmlAttribute
     private String material;
 
     Wheel(String material) {
