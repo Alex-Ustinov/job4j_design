394b5a60bc722a5c8dead1791293ab9e74b6b57c
18485888 <AAlUstinov@sberbank.ru>
2021-04-06 00:58:45 +0300

    Что такое база данных

---
+++ src/test_db.sql
@@ -0,0 +1,10 @@
+-- create table car_types (
+-- 	id serial primary key,
+-- 	brand varchar (100),
+-- 	range_cars integer 
+-- )
+--insert into car_types(brand, range_cars) values ('kia', 100)
+--update car_types set brand = 'mercedes-benz' where id = '1'
+--delete from car_types where id = '1'
+
+
