cb5a9db8f049faa23cbcb1cd1c8aa203c4399b5b
18485888 <AAlUstinov@sberbank.ru>
2021-04-21 18:26:19 +0300

    right left join

--- join_left_right.sql
+++ join_left_right.sql
@@ -13,6 +13,7 @@ create table employees (
 insert into departments(name) values ('developers');
 insert into departments(name) values ('supports');
 insert into departments(name) values ('accounters');
+insert into departments(name) values ('administrators');
 
 insert into employees(name, department_id) values ('alex', 1);
 insert into employees(name, department_id) values ('sam', 2);
@@ -26,6 +27,56 @@ insert into employees(name, department_id) values ('adam', 3);
 insert into employees(name, department_id) values ('mat', 3);
 
 select * 
+from employees as e 
+left join departments as d 
+on e.department_id = d.id 
+
+select * 
+from departments as d 
+right join employees as e 
+on e.department_id = d.id 
+
+select * 
+from employees as e 
+full join departments as d 
+on e.department_id = d.id 
+
+select * 
+from employees as e 
+cross join departments as d 
+
+select * 
 from departments as d 
 left join employees as e 
+on e.department_id = d.id
+where department_id is null
+
+
+select * 
+from employees as e 
+left join departments as d 
 on e.department_id = d.id 
+
+select * 
+from departments as d 
+right join employees as e 
+on e.department_id = d.id 
+
+create table teens (
+	id serial primary key,
+	name varchar(255),
+	gender varchar(255)
+);
+
+insert into teens(name, gender) values ('alex', 'male');
+insert into teens(name, gender) values ('sam', 'male');
+insert into teens(name, gender) values ('pam', 'female');
+insert into teens(name, gender) values ('koly', 'male');
+insert into teens(name, gender) values ('dim', 'male');
+insert into teens(name, gender) values ('anton', 'male');
+insert into teens(name, gender) values ('zina', 'female');
+insert into teens(name, gender) values ('susana', 'female');
+insert into teens(name, gender) values ('adam', 'male');
+insert into teens(name, gender) values ('mat', 'male');
+
+select * from teens t1 cross join teens t2;
